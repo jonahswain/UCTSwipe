db19916b0d63314895fc5065d5186114f590da18
Jonah Swain <jonahswain@gmail.com>
2018-06-19 14:05:01 +0200

    Added LCD file

---
+++ UCTSwipe/UCTSwipe/RPI_LCD.py
@@ -0,0 +1,20 @@
+# UCTSwipe
+# Student access card reader/logger
+# Author: Jonah Swain (SWNJON003)
+# LCD
+
+# Imports
+import gpiozero
+
+class RPI_LCD(object):
+    """Module to allow use of the ADM1602K-NSA-FBS/3.3V LCD"""
+
+    def __init__(self, pin_RS, pin_EN, pin_D4, pin_D5, pin_D6, pin_D7):
+        pass
+
+    def __init__(self, pin_RS, pin_EN, pin_D0, pin_D1, pin_D2, pin_D3, pin_D4, pin_D5, pin_D6, pin_D7):
+        pass
+
+
+
+
--- UCTSwipe/UCTSwipe/main.py
+++ UCTSwipe/UCTSwipe/main.py
@@ -1,4 +1,4 @@
-
 # UCTSwipe
 # Student access card reader/logger
 # Author: Jonah Swain (SWNJON003)
+# Main file

be0173d5992cb5853519b6fe16194bd5fa8adc54
Jonah Swain <jonahswain@gmail.com>
2018-06-20 15:21:29 +0200

    Wrote some LCD test code

--- UCTSwipe/UCTSwipe/main.py
+++ UCTSwipe/UCTSwipe/main.py
@@ -2,3 +2,12 @@
 # Student access card reader/logger
 # Author: Jonah Swain (SWNJON003)
 # Main file
+
+import RPI_LCD
+import time
+import gpiozero as GPIO
+
+def testLCD():
+    myLCD = RPI_LCD.LCD(20, 21, 5, 6, 13, 19)
+    myLCD.initialise()
+    myLCD.write("Testing LCD", "123456789")
